4e694e04715bf51f16e53481c0bf4cf0e873d819
Miles D <milesoftrains@gmail.com>
2026-09-20 00:28:41 -0500

    Add drive times and improve travel page style

--- wedding-website-react/src/TravelInfo.jsx
+++ wedding-website-react/src/TravelInfo.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const TravelInfo = () => {
   const [airports, setAirports] = useState([]);
@@ -52,13 +54,15 @@ const handleChange = (event) => {
   return (
     <div>
 
+      <Link to="/" style={{color: 'black'}}>Back to Home</Link>
+
       <h1>Travel Information</h1>
 
-      <h2>Block Hotel Rooms</h2>
+      <h2 style={{marginTop: "60px", marginBottom: "20px"}}>Block Hotel Rooms</h2>
       <p>There is a strong possibility that we will have block hotel rooms available in Nauvoo for family and friends traveling long distances. More information to come. Contact Hannah or Miles if you think you will need a hotel for the night before and/or after the wedding. </p>
 
 
-      <h2>Closest Airports</h2>
+      <h2 style={{marginTop: "60px"}}>Closest Airports</h2>
 
       <label style={{ display: 'flex', flexDirection: 'column', gap: '8px', alignItems: 'flex-end'}}>
         <select className="travel-sort" value={selectedValue} onChange={handleChange}>
@@ -73,8 +77,9 @@ const handleChange = (event) => {
       {error && <p>{error}</p>}
       {!isLoading && !error && sortedAirports.map((airport) => (
         <div className="airport-card" key={airport.name}>
-          <h4>{airport.name} ({airport.code})</h4>
+          <h3>{airport.name} ({airport.code})</h3>
           <p>{airport.location} - {airport.milesAway} miles from Nauvoo </p>
+          <p>Estimated drive time: {airport.driveTime}</p>
           {airport.note && <p style={{fontStyle:"italic"}}>{airport.note}</p>}
         </div>
       ))}
